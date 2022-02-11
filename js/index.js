8355afe49b5475520977b73680d805cd4933ab9e
sarthur7 <sarthur7@gmail.com>
2022-02-10 19:45:14 -0700

    HW19 2/10/22 7:45pm
    HW19 2/10/22 7:45pm

---
+++ js/index.js
@@ -0,0 +1,11 @@
+console.log("Your index.js file is loaded correctly!");
+
+$(":button").hover(function(){
+    if (this).mouseenter("background-color, "#131F32") {
+        
+    } else {
+        
+    }
+    $(this).mouseenter("background-color", "#131F32");
+    $(this).mouseleave("background-color", "#2D656D");
+});
